77c4c0db209c4996ffd389a83aba0167aace4630
Dhruv1249 <dhr1249.lm@gmail.com>
2026-01-23 15:36:49 +0530

    Updated server.js with /request and /login api paths

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json()); // Middleware
 
+users = [];
+
 // Post request ending with /register
 app.post('/register', (request, response) =>{
   const {name, email, password } = request.body;
@@ -14,6 +16,15 @@ app.post('/register', (request, response) =>{
       message: 'Name, Email, Password all are required.'
     });
   }
+  
+  // Return status 401 if email already exists
+  const emailExists = users.find(user => user.email === email);
+  // find methods returns the object where it is present not true or false
+  if(emailExists){
+    return response.status(401).json({
+      message: `User already exists for the email: ${email}`
+    });
+  };
 
   const newUser = {
     id: users.length + 1,
@@ -27,7 +38,30 @@ app.post('/register', (request, response) =>{
   // Return status 200 when regsitration is successfully done
   return response.status(200).json({
     message: "Successfully registered",
-    id: { newUser.id }
+    user: {id: newUser.id} 
+  });
+
+});
+
+app.post('/login', (request, response) => {
+  const {email, password} = request.body;
+  if (!email && !password){
+    return response.status(400).json({
+      message: "Please enter both email and passwor",
+    });
+  }
+
+  const hasAccount = users.find( 
+    user => user.password === password && user.email === email
+  );
+  if (hasAccount){
+    return response.status(200).json({
+      message: "Successfully logged in!",
+    });
+  }
+  
+  return response.status(400).json({
+    message: "Account doesn't exist!",
   });
 
 });
