3ccaa20a69824896153bb9c104dc358449800c3d
Dhruv1249 <dhr1249.lm@gmail.com>
2026-01-24 10:46:44 +0530

    Refactored code for the server

--- server.js
+++ server.js
@@ -1,70 +1,11 @@
 const express = require('express');
+const authRoutes = require('./src/routes/authRoutes') 
 
 const app = express();
 
 app.use(express.json()); // Middleware
 
-users = [];
-
-// Post request ending with /register
-app.post('/register', (request, response) =>{
-  const {name, email, password } = request.body;
-  
-  // Return status 400 (client error) if a field it missing
-  if (!name || !email || !password){
-    return response.status(400).json({
-      message: 'Name, Email, Password all are required.'
-    });
-  }
-  
-  // Return status 401 if email already exists
-  const emailExists = users.find(user => user.email === email);
-  // find methods returns the object where it is present not true or false
-  if(emailExists){
-    return response.status(401).json({
-      message: `User already exists for the email: ${email}`
-    });
-  };
-
-  const newUser = {
-    id: users.length + 1,
-    name: name,
-    email: email,
-    password: password
-  }
-
-  users.push(newUser);
-  
-  // Return status 200 when regsitration is successfully done
-  return response.status(200).json({
-    message: "Successfully registered",
-    user: {id: newUser.id} 
-  });
-
-});
-
-app.post('/login', (request, response) => {
-  const {email, password} = request.body;
-  if (!email && !password){
-    return response.status(400).json({
-      message: "Please enter both email and passwor",
-    });
-  }
-
-  const hasAccount = users.find( 
-    user => user.password === password && user.email === email
-  );
-  if (hasAccount){
-    return response.status(200).json({
-      message: "Successfully logged in!",
-    });
-  }
-  
-  return response.status(400).json({
-    message: "Account doesn't exist!",
-  });
-
-});
+app.use('/auth', authRoutes);
 
 app.listen(5001, () =>{
   console.log('Server is running on port 5001');
